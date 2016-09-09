5cc9fbf8abc540c5e67e784446db1b8126c557d9
2hwooo727@gmail.com <2hwooo727@gmail.com>
2016-09-09 11:06:02 +0900

    cursor exe

--- samp.sql
+++ samp.sql
@@ -7,3 +7,18 @@ BEGIN
     SELECT major_seq,title FROM major;
 END select_major;
 
+-- EXE_SELECT_MAJOR(ALL)
+DECLARE
+  l_cursor  SYS_REFCURSOR;
+  l_major_seq   major.major_seq%TYPE;
+  l_title   major.title%TYPE;
+BEGIN
+  select_major (l_cursor);         
+  LOOP 
+    FETCH l_cursor
+    INTO  l_major_seq, l_title;
+    EXIT WHEN l_cursor%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE(l_major_seq || ' | ' ||l_title);
+  END LOOP;
+  CLOSE l_cursor;
+END;
